c11d28619dcbb7c55759ac186648a77fd1841704
Maija Lehtosaari <maija.lehtosaari@student.lut.fi>
2025-02-26 17:33:44 +0200

    Formatted code

--- client/src/components/Kanban.tsx
+++ client/src/components/Kanban.tsx
@@ -1,166 +1,209 @@
 import { useEffect, useState } from "react";
-import {useParams} from 'react-router-dom'
-import {Grid,Typography, Paper,TextField, Button,Box} from '@mui/material/';
+import { useParams } from "react-router-dom";
+import {
+  Grid,
+  Typography,
+  Paper,
+  TextField,
+  Button,
+  Box,
+} from "@mui/material/";
+//import { DndContext, useDraggable, useDroppable } from '@dnd-kit/core';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 interface Column {
-    _id: string;
-    name: string;
-    tickets: {_id: string; name: string} [];
+  _id: string;
+  name: string;
+  tickets: { _id: string; name: string }[];
 }
 
 const Kanban = () => {
-    const {boardId } = useParams<{boardId:string }>();
-    const [columns, setColumns] = useState<Column[]>([]);
-    const [newColumnName, setNewColumnName] = useState("");
-    const [newTicketName, setNewTicketName] =  useState<{ [key: string]: string }>({});
+  const { boardId } = useParams<{ boardId: string }>();
+  const [columns, setColumns] = useState<Column[]>([]);
+  const [newColumnName, setNewColumnName] = useState("");
+  const [newTicketName, setNewTicketName] = useState<{ [key: string]: string }>(
+    {}
+  );
 
-    // Fetch columns & tickets
-    const fetchColumns = async() =>{
-        const token = localStorage.getItem("token");
-        try{
-            const response = await fetch(`http://localhost:3000/api/columns/${boardId}`, {
-                method: "GET",
-                headers: {"Authorization": `Bearer ${token}`, "Content-type": "application/json" }
-            })
-            if (!response.ok) {
-                throw new Error("Error fetching columns");
+  // Fetch columns & tickets
+  const fetchColumns = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/columns/${boardId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Error fetching columns");
+      }
+      const columnData = await response.json();
+      const columnsList = columnData.columns;
+      const columnsWithTickets = await Promise.all(
+        columnsList.map(async (column: Column) => {
+          const ticketResponse = await fetch(
+            `http://localhost:3000/api/tickets/${column._id}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-type": "application/json",
+              },
             }
-            const columnData = await response.json();
-            const columnsList = columnData.columns;
-            const columnsWithTickets = await Promise.all(
-                columnsList.map(async(column: Column) =>  {
-                    const ticketResponse = await fetch(`http://localhost:3000/api/tickets/${column._id}`, {
-                        method: "GET",
-                        headers: {"Authorization": `Bearer ${token}`, "Content-type": "application/json" }
-                })
-                if (!ticketResponse.ok) {
-                    throw new Error("Error fetching tickets");
-                }
-            const ticketData = await ticketResponse.json();
-            const ticketsList = ticketData.tickets;
+          );
+          if (!ticketResponse.ok) {
+            throw new Error("Error fetching tickets");
+          }
+          const ticketData = await ticketResponse.json();
+          const ticketsList = ticketData.tickets;
 
-            return {...column, tickets:ticketsList};
-            })
+          return { ...column, tickets: ticketsList };
+        })
+      );
+      setColumns(columnsWithTickets);
+    } catch (error) {
+      console.log(`Error fetching columns, ${error.message}`);
+    }
+  };
+  // useEffect that calls fetchColumns when boardId is updated
+  useEffect(() => {
+    fetchColumns();
+  }, [boardId]);
+
+  // Create a new column
+  const createColumn = async () => {
+    if (!newColumnName.trim()) {
+      return;
+    }
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("http://localhost:3000/api/columns", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newColumnName, boardId: boardId }),
+      });
+      if (!response.ok) {
+        throw new Error("Error creating column");
+      }
+      const newColumn: Column = await response.json();
+      setColumns([...columns, newColumn]);
+      fetchColumns();
+      setNewColumnName("");
+    } catch (error) {
+      console.log(`Error creating column, ${error.message}`);
+    }
+  };
+
+  // Create a new ticket
+  const createTicket = async (columnId: string) => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch("http://localhost:3000/api/tickets", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newTicketName[columnId], columnId }),
+      });
+      if (!response.ok) {
+        throw new Error("Error creating ticket");
+      }
+      const newTicket = await response.json();
+      // add new ticket to the right column in the state
+      setColumns((prevColumns) =>
+        prevColumns.map(
+          (column) =>
+            column._id === columnId
+              ? { ...column, tickets: [...column.tickets, newTicket.newTicket] }
+              : column // column id didn't match --> dont change array
         )
-            setColumns(columnsWithTickets);
-        } catch (error) {
-            console.log(`Error fetching columns, ${error.message}`);
-        }
-    }
-    // useEffect that calls fetchColumns when boardId is updated
-    useEffect(() => {
-        fetchColumns();
-    }, [boardId])
+      );
 
-    // Create a new column
-    const createColumn = async () => {
-        if (!newColumnName.trim()){
-            return;
-        }
-        const token = localStorage.getItem("token")
-        try {
-            const response = await fetch("http://localhost:3000/api/columns",{
-                method: "POST",
-                headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"},
-                body: JSON.stringify({name: newColumnName, boardId: boardId})
-            })
-            if (!response.ok) {
-                throw new Error("Error creating column");
-            }
-            const newColumn: Column = await response.json();
-            setColumns([...columns, newColumn]);
-            fetchColumns();
-            setNewColumnName("");
-        } catch (error) {
-            console.log(`Error creating column, ${error.message}`);
-        }
+      setNewTicketName("");
+    } catch (error) {
+      console.log(`Error creating ticket, ${error.message}`);
     }
-    
-    // Create a new ticket
-    const createTicket = async (columnId: string) => {
-        const token = localStorage.getItem("token")
-        try {
-            const response = await fetch("http://localhost:3000/api/tickets",{
-                method: "POST",
-                headers: {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"},
-                body: JSON.stringify({name: newTicketName[columnId], columnId})
-            })
-            if (!response.ok) {
-                throw new Error("Error creating ticket");
-            }
-            const newTicket = await response.json();
-            // add new ticket to the right column in the state
-            setColumns((prevColumns) => prevColumns.map((column) =>
-                column._id === columnId
-                ? { ...column, tickets: [...(column.tickets), newTicket.newTicket] }
-                :column // column id didn't match --> dont change array
-        ));
+  };
+  /*
+   * TicketNameChange is called when input field is changed
+   * It updates the newTicketName state with the tickets name for a specific column
+   */
+  const TicketNameChange = (columnId: string, name: string) => {
+    setNewTicketName((prev) => ({ ...prev, [columnId]: name }));
+  };
 
-        setNewTicketName("");
-        } catch (error) {
-            console.log(`Error creating ticket, ${error.message}`);
-        }
-    }
-    /*
-    * TicketNameChange is called when input field is changed
-    * It updates the newTicketName state with the tickets name for a specific column
-    */
-    const TicketNameChange = (columnId: string, name: string) => {
-        setNewTicketName((prev) => ({ ...prev, [columnId]: name }));
-      };
-
-    return (
+  return (
     <div>
-    <Box sx={{display:"flex", margin:2}}>
-
-        <TextField label="New column name" variant="outlined" value={newColumnName} onChange={(e) => setNewColumnName(e.target.value)}
-        sx={{ margin:1}}
+      <Box sx={{ display: "flex", margin: 2 }}>
+        <TextField
+          label="New column name"
+          variant="outlined"
+          value={newColumnName}
+          onChange={(e) => setNewColumnName(e.target.value)}
+          sx={{ margin: 1 }}
         />
         <Button onClick={createColumn}> Add a column </Button>
-    </Box>
-    <Grid container spacing={2}>
+      </Box>
+      <Grid container spacing={2}>
         {columns.map((column) => (
-            <Grid item key={column._id}>
-            <Paper 
-            sx={{
-                width:100,
-                padding:2,
-                display: 'flex',
-                border: '1px solid blue',
-                margin: 1,
-                flexDirection: "column"
-            }}
-            >
-            <Typography variant="h6" sx={{textAlign: 'center', fontSize: 15,}}>
-            {column.name}
-            </Typography>
-            <div style={{flexGrow: 1, overflowY: "auto", padding: 5}}>
-            {column.tickets?.map((ticket) => (
+          <Grid item key={column._id}>
             <Paper
-                key={ticket._id}
-                sx={{
-                    padding:2,
-                    border: '1px solid blue',
-                    marginBottom: 1,
-                    minHeight: 50,
-                    maxHeight: 50,
-                }}
-                >
-                    {/*Ticket title*/}    
-                    <Typography sx={{fontSize:12}}>{ticket.name}</Typography>
+              sx={{
+                width: 100,
+                padding: 2,
+                display: "flex",
+                border: "1px solid blue",
+                margin: 1,
+                flexDirection: "column",
+              }}>
+              <Typography
+                variant="h6"
+                sx={{ textAlign: "center", fontSize: 15 }}>
+                {column.name}
+              </Typography>
+              <div style={{ flexGrow: 1, overflowY: "auto", padding: 5 }}>
+                {column.tickets?.map((ticket) => (
+                  <Paper
+                    key={ticket._id}
+                    sx={{
+                      padding: 2,
+                      border: "1px solid blue",
+                      marginBottom: 1,
+                      minHeight: 50,
+                      maxHeight: 50,
+                    }}
+                  >
+                    {/*Ticket title*/}
+                    <Typography sx={{ fontSize: 12 }}>{ticket.name}</Typography>
+                  </Paper>
+                ))}
+              </div>
+              <TextField
+                label="new ticket"
+                variant="outlined"
+                value={newTicketName[column._id] || ""}
+                onChange={(e) => TicketNameChange(column._id, e.target.value)}
+                sx={{ margin: 1 }}
+              />
+              <Button onClick={() => createTicket(column._id)}>
+                {" "}
+                Add a ticket{" "}
+              </Button>
             </Paper>
-            ))}
-            </div>
-            <TextField label="new ticket" variant="outlined" value={newTicketName[column._id]|| ""} onChange={(e) => TicketNameChange(column._id, e.target.value)}
-            sx={{ margin:1}}
-            />
-            <Button onClick={() => createTicket(column._id)}> Add a ticket </Button>
-            </Paper>
-            </Grid>
+          </Grid>
         ))}
-        </Grid>
-        </div>
-    )
-}
+      </Grid>
+    </div>
+  );
+};
 
 export default Kanban;
